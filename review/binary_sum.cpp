6f838777075a9c320855cb6fab31d9cd7cb04012
Alex Gaesser <agaesser@gmail.com>
2017-10-18 17:56:14 -0700

    first commit

---
+++ review/binary_sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+// write a function sum(n) that returns the sum of bits in the binary representation of n
+// for example, sum(6) returns 1+1+0 = 2
+// write both iterative and recursive solutions
+
+
+int main() {
+
+	return 0;
+}
